8f905a1b7aaaed900d9e3bd0a14352bd199f7261
Daniel Cano <anuvis96@gmail.com>
2025-05-04 02:40:41 -0500

    Primer commit: estructura base del proyecto

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('registro').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Gracias por inscribirte. Pronto nos pondremos en contacto contigo.');
+  });
+  
